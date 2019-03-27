fb63fcc363f5683df176f5c888e3ef018f2d3a9f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-03-27 09:11:01 +0100

    Modifica nome test

---
+++ CardioAnalisi/DataCardio.Test/DataCardio1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataCardio.Test
+{
+    [TestClass]
+    public class DataCardio1
+    {
+        [TestMethod]
+        public void TestCardio1()
+        {
+
+
+        }
+    }
+}
---
+++ CardioAnalisi/DataCardio.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataCardio.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DataCardio.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("88caaf9f-d051-40ba-959e-d80d8991c9eb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
